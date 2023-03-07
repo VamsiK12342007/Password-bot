7aa88ba5b2dc9b54694c38cf99372627dfe5aaf3
Vamsi Kurli <n00466003@humbermail.com>
2023-03-07 00:28:42 -0500

    Did some changes to responses

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -1,27 +1,33 @@
-function handlesubmit(){
+function handlesubmit() {
     event.preventDefault();
-    document.getElementById("results").innerHTML = ""
+    document.getElementById("results").innerHTML = "";
     let userinput = document.getElementById("inputbox").value;
-    if(userinput.length<8){
-        document.getElementById("results").innerHTML = "The password should be exacyly be 8 characters";
-    }
-
-    if(userinput.length>8){
-        document.getElementById("results").innerHTML += "<br/>The password should be exactly be 8 characters";
-    }
-
-    let containsCapital = false;
-    Array.from(userinput).forEach(c => {
-        if(c.match(/[a-z]/i) && c===c.toUpperCase()){
-            containsCapital = true;
-        }
-    });
-    if(!containsCapital){
-        document.getElementById("results").innerHTML += "<br/>You need to have atleast 1 uppercase characters";
-    } 
-    if(!(userinput.includes("!") || userinput.includes("@") || userinput.includes("#") || userinput.includes("$"))){
-        document.getElementById("results").innerHTML += "<br/>You need to have one of these symbols (!@#$)";
+    let resultMessage = ""
+    
+    if (userinput.length < 8) {
+    resultMessage = "The password should be exactly be 8 characters";
     }
     
+    if (userinput.length > 8) {
+    resultMessage += "The password should be exactly be 8 characters";
+    }
+    
+    let containsCapital = false;
+    Array.from(userinput).forEach((c) => {
+    
+        if (c.match(/[a-z]/i) && c === c.toUpperCase()) {
+    containsCapital = true;
+    }
+    });
+    
+    if (!containsCapital) {
+    resultMessage += "<br/>You need to have atleast 1 uppercase characters";
+    }
+    
+    if (!(userinput.includes("!") || userinput.includes("@") || userinput.includes("#") || userinput.includes("$"))) {
+    resultMessage += "<br/>You need to have one of these symbols (!@#$)";
+    }
+    
+    if(resultMessage == "") resultMessage = "Bravo! You win"
+    document.getElementById("results").innerHTML = resultMessage;
 }
-
